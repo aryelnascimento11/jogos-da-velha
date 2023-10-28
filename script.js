9b2eb708a446d3cb1d66fe8e45be666192200ef9
aryelnascimento11 <aryeln897@gmail.com>
2023-10-27 21:22:51 -0300

    treinando

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 let jogador = "X"
 
-
-function marcar(id) {
+function marcar(id){
     let celula = document.getElementById(id)
-    
-     
+
     if(celula.innerHTML === ""){
         celula.innerHTML = jogador
-    } else{
+    }else{
         return
     }
-
-    if (jogador == "X") {
+    if(jogador == "X"){
         jogador = "O"
-    } else {
-        jogador = "X"    
+    } else{
+        jogador = "X"
     }
 }
- //  === valor de comparar
+
+() => {
+    isCircleTurn = false;
+  
+    for (const cell of cellElements) {
+      cell.classList.remove("circle");
+      cell.classList.remove("x");
+      cell.removeEventListener("click", handleClick);
+      cell.addEventListener("click", handleClick, {
+        once: true
+      });
+    }
+  
+    setBoardHoverClass();
+    winningMessage.classList.remove("show-winning-message");
+  }
